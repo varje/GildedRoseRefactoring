3b813f5530024386d4044e8318f64045109a7dd3
Varje Kass <varjekass@gmail.com>
2020-07-07 17:12:53 +0300

    create testcases

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,37 +1,49 @@
 # -*- coding: utf-8 -*-
 import unittest
+import copy
 
 from gilded_rose import Item, GildedRose
+# from test_items import mock_items
 
 class GildedRoseTest(unittest.TestCase):
     def setUp(self):
-        self.mock_items = [
+        mock_normal_items = [
             Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
             Item(name="Aged Brie", sell_in=2, quality=0),
             Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
-            Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-            Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-            Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-            Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+            Item(name="Conjured Mana Cake", sell_in=3, quality=6)  
         ]
+        #     Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        #     Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+            
+        #     Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        #     Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        #     Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        self.mock_normal_items = mock_normal_items
 
     def test_foo(self):
         items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
         self.assertEqual("foo", items[0].name)
 
     def test_mock_items_array_length(self):
-        items = self.mock_items
-        self.assertEqual(len(items), 9)
+        items = self.mock_normal_items
+        self.assertEqual(len(items), 4)
 
     def test_each_day_denoting_Sellin_value(self):
-        pass
+        original_items = self.mock_normal_items
+        updated_items = copy.deepcopy(original_items)
+        original_obj = GildedRose(original_items)
+        updated_obj = GildedRose(updated_items)
+        updated_obj.update_quality()
+        self.assertEqual(([item.sell_in-1 for item in original_items]), ([item.sell_in for item in updated_items]))
 
     def test_each_day_denoting_Quality_value(self):
-        pass
+        original_items = self.mock_normal_items
+        updated_items = copy.deepcopy(original_items)
+        original_obj = GildedRose(original_items)
+        updated_obj = GildedRose(updated_items)
+        updated_obj.update_quality()
+        self.assertEqual(([item.quality-1 for item in original_items]), ([item.quality for item in updated_items]))
 
 
     def test_quality_degrades_twice_as_fast_after_sell_date_passed(self):
