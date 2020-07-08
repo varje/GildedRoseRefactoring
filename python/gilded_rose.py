d9a2a7ce2ec9a2ff3a5dea52331efd8bf21df3b6
Varje Kass <varjekass@gmail.com>
2020-07-08 17:43:42 +0300

    seperate quality and sell_in days update

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -15,50 +15,60 @@ class GildedRose(object):
         # as by task description can understand that might be eg. "Conjured Aged Brie"
         conjured = "Conjured"
 
+        # help functions
+        def decrease_sellin_days(item):
+            item.sell_in -= 1
+
         def decrease_quality(item, points):
             item.quality -= points
 
         def increase_quality(item, points):
             item.quality -= points
 
-        def decrease_sellin_days(item, days):
-            item.sell_in -= days
-
         def backstage_passes_update(item):
             # "Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches;
             # Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less 
             # but Quality drops to 0 after the concert
-            if item.sell_in <= 0:
+            if item.sell_in < 0:
                 item.quality = 0
             elif item.sell_in <= 5:
-                increase_quality *= 3
+                increase_quality(item, 3)
             elif item.sell_in <= 10:
-                increase_quality *= 2
-
+                increase_quality(item, 2)
+            else:
+                increase_quality(item, 1)
+        
+        def change_quality(item):
+            if item.quality <= 0:
+                # The Quality of an item is never negative
+                # although item quality can not go below 0, this is to catch items which start with below 0 quality
+                item.quality == 0
+            elif item.quality >= 50 or item == sulfuras:
+                # The Quality of an item is never more than 50
+                # "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+                pass
+            elif item == brie:
+                # "Aged Brie" actually increases in Quality the older it gets
+                increase_quality(item, 1)
+            elif item == backstage_passes:
+                # as backstage_passes have quite complicated rules, will make subfunction
+                backstage_passes_update(item)
+            elif item == conjured:
+                # "Conjured" items degrade in Quality twice as fast as normal items
+                decrease_quality(item, 2)
+            elif item.sell_in < 0:
+                # Once the sell by date has passed, Quality degrades twice as fast
+                # will assume that this goes only for normal items, not conjured
+                decrease_quality(item, 2)
+            else:
+                # normal case
+                decrease_quality(item, 1)
 
         for item in self.items:
             # check exceptions
-            if item == sulfuras:
-                # "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-                pass
-            if item == brie:
-                # "Aged Brie" actually increases in Quality the older it gets
-                increase_quality(item, 1)
-            if item == backstage_passes:
-                backstage_passes_update(item)
-            if item == conjured:
-                # "Conjured" items degrade in Quality twice as fast as normal items
-                decrease_quality(item, 2)
-            if item.quality == 0 or item.quality >= 50:
-                # The Quality of an item is never negative; The Quality of an item is never more than 50
-                decrease_sellin_days(item, 1)
-            if item.sell_in < 0:
-                # Once the sell by date has passed, Quality degrades twice as fast
-                decrease_quality(item, 2)
-
-            # seperate quality and sellin changes, add extra parameter for exceptions
-            decrease_quality(item, points)
-            decrease_sellin_days(item, days)
+            # as exceptions go only for quality, will call out two subfunctions seperately
+            decrease_sellin_days(item)
+            change_quality(item)
 
 
 
